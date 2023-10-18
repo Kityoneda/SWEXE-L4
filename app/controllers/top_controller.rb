dfba286c0575bbbc64eae3b4dbb9602f0504fcc5
Kazuki Yoneda <2111140039c@ed.fuk.kindai.ac.jp>
2023-10-18 07:24:15 +0000

    second commit

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def main
-    if session[:login_uid]!= nil
+    if session[:login_uid] != nil
       render "main"
     else
       render "login"
@@ -8,6 +8,7 @@ class TopController < ApplicationController
   end
 
   def login
+    logger.debug params[:uid]
     if params[:uid] == 'kindai' and params[:pass] == 'sanriko'
       session[:login_uid] = params[:uid]
       redirect_to top_main_path
